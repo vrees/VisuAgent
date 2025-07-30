78a522eaf8d8fe89631a9802eb25596de69d2959
vrees <viktor.rees@gmx.de>
2025-07-30 15:47:35 +0200

    fix(frontend): entferne doppelte Importe und Initialisierung in test.ts (Angular Test-Bootstrap)

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -10,17 +10,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 
-// Alle .spec.ts-Dateien automatisch laden (Angular 16+)
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
-
 declare const require: any;
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
 const context = require.context('./', true, /\.spec\.ts$/);
 context.keys().map(context);
